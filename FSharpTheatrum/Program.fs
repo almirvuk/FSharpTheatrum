b1e0abe95476ea8adbc3cff7a17258f1a9ec23bc
almir.vuk <almir.vuk@outlook.com>
2020-03-07 17:14:25 +0100

    working with classes and user input.

--- FSharpTheatrum/Program.fs
+++ FSharpTheatrum/Program.fs
@@ -2,7 +2,30 @@
 
 open System
 
+type Person(name: string, birthDay: DateTime) = 
+
+    member private this.Name = name
+    member private this.BirthDay = birthDay
+    member this.IntroduceMe() = "I am " + this.Name + ", and I am " + this.BirthDay.Year.ToString() + " years old." 
+
+let hello() =
+    printf "What is your name? "
+
+    let name = Console.ReadLine()
+
+    printf "The year I was born? "
+    let year = Console.ReadLine()
+
+    let person = new Person(name, new DateTime(Int32.Parse(year), 1, 1))
+
+    person
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+
+    let person = hello();
+
+    printfn "%s" (person.IntroduceMe())
+
     0 // return an integer exit code
