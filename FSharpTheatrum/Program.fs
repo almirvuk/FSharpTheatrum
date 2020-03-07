64681764655e8f465e47c783efe3758335c78a8c
almir.vuk <almir.vuk@outlook.com>
2020-03-07 17:37:34 +0100

    code formatting.

--- FSharpTheatrum/Program.fs
+++ FSharpTheatrum/Program.fs
@@ -6,18 +6,17 @@ type Person(name: string, birthDay: DateTime) =
 
     member private this.Name = name
     member private this.BirthDay = birthDay
-    member this.IntroduceMe() = "I am " + this.Name + ", and I am " + this.BirthDay.Year.ToString() + " years old." 
+    member this.IntroduceMe() = "I am " + this.Name + ", and I am " + (DateTime.Now.Year - this.BirthDay.Year).ToString() + " years old." 
 
 let hello() =
-    printf "What is your name? "
 
+    printf "What is your name? "
     let name = Console.ReadLine()
 
     printf "The year I was born? "
     let year = Console.ReadLine()
 
     let person = new Person(name, new DateTime(Int32.Parse(year), 1, 1))
-
     person
 
 [<EntryPoint>]
